157941f3bddae2ba1d61c3d5207c166d294ec0d0
Danni4421 <85418881+Danni4421@users.noreply.github.com>
2023-02-18 22:53:45 +0700

    generate response weather

--- src/script/script.js
+++ src/script/script.js
@@ -1,45 +1,57 @@
 import Weather from './weather.js';
 import info from '../data/data.js';
 
-const continer = document.getElementById('container')
+const container = document.getElementById('container')
 const btnSearch = document.getElementById('btnSearch')
-const searchValue = document.getElementById('search');
+const searchValue = document.getElementById('search')
+const containerWeather = document.createElement('div')
+const titleState = document.createElement('h3')
 const dataGeo = [];
 
-document.addEventListener('DOMContentLoaded', e => {
-
-})
-
-
 btnSearch.addEventListener('click', e => {
- getCoordinate();
+  getCoordinate();
 })
 
 const getCoordinate = async () => {
- const result = await getLatitudeLongitude();
- console.log(result);
+  await getLatitudeLongitude();
 }
 
 const getLatitudeLongitude = () => {
- getDataWeather(searchValue.value, info.data.APIKey);
- return new Promise((resolve, reject) => {
-  setTimeout(() => {
-   if (dataGeo !== null) {
-    resolve('Berhasil mendapatkan data geograpich');
-   } else {
-    reject('Gagal mendapatkan data geograpich');
-   }
-  }, 1500);
- });
+  getDataWeather(searchValue.value, info.data.APIKey);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (dataGeo !== null) {
+        resolve('Berhasil mendapatkan data geograpich');
+        getWeather();
+      } else {
+        reject('Gagal mendapatkan data geograpich');
+      }
+    }, 500);
+  });
 }
 
 
 const getDataWeather = (searchValue, key) => {
- fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=1&appid=${key}`)
-  .then(response => response.json())
-  .then(res => {
-   res.map(e => {
-    dataGeo.push(e);
-   });
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=1&appid=${key}`)
+    .then(response => response.json())
+    .then(res => {
+      res.map(e => {
+        dataGeo.push(e);
+      });
+    });
+};
+
+const getWeather = () => {
+  const [result] = dataGeo;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${result.lat}&lon=${result.lon}&appid=${info.data.APIKey}`)
+    .then(response => response.json())
+    .then(result => info.data.result.push(result));
+  setContainerWeather();
+};
+
+const setContainerWeather = () => {
+  // titleState.innerHTML = info.data.result.main;
+  info.data.result.map(response => {
+    console.log(response);
   });
 };
